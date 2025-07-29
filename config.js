1cb52872aa8b41403e8ec9758b8a6a0b94db0c0d
SeptimusVII <vansteene.quentin@gmail.com>
2025-07-29 17:59:08 +0200

    add config

--- config.js
+++ config.js
@@ -1,1 +1,33 @@
-module.exports = {};
+module.exports = {
+	'radius': '60px',
+	'colors':{
+		'yellow' : '#EABF7C',
+		'blacklight': '#1D1D1B'
+	},
+	'primary': 'colors(yellow)',
+	'body':{
+		'background': 'colors(blacklight)',
+		'font-color': 'colors(white)',
+		'block-background': 'colors(none)'
+	},
+	'btn':{
+		// 'background': 'primary',
+		// 'font-color': 'contrastFW(colors(white),primary)',
+		// 'border-size': 'border-default-size',
+		// 'radius': true,
+		'uppercase': true,
+		// 'font-weight': 300,
+	},
+	'container':{
+		'xl'  : '1280px', // 1140px
+		// 'lg'  : '960px', // 1140px
+		// 'md'  : '960px',
+		// 'sm'  : '720px',
+		// 'xs'  : '540px',
+		// 'xxs' : '100%',
+	},
+	'footer': {
+		'background': 'body(background)',
+		'font-color': 'body(font-color)',
+	},
+};
